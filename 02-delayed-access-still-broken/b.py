db660627114843f2e9e577b174ca0d0a47109d47
Marwan Aljubeh <marwan@openai.com>
2024-10-03 01:43:56 +0100

    Delayed access (fully working)
    A quick search online will yield a few options for how to deal with circular type dependencies. Those include:
    
    1. Wrapping the type in quotes.
    2. Enabling postponed evaluation.
    
    Those two methods work pretty much the same way: Type annotations are treated
    by Python as strings until they are executed, and only the would the actual
    type get evaluated.
    
    For simplicity's sake, let's just wrap the type in quotes.
    
    Once we do, we finally have everything a working example.
    
    Running `main.py` works as expected and prints:
    ```
    B(1)
    A(2)
    ```

--- 02-delayed-access-still-broken/b.py
+++ 02-delayed-access-still-broken/b.py
@@ -5,7 +5,7 @@ class B:
     def __init__(self, value: int):
         self.value = value
 
-    def get_a(self) -> a.A:
+    def get_a(self) -> "a.A":
         return a.A(self.value)
 
     def __str__(self) -> str:
